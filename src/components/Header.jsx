648a26645142e4c280992c2c3f3e9654dc2585a6
Killian Portier <killian.portier@yahoo.fr>
2025-01-06 19:32:11 +0100

    feat: Add tailwind and config it

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="text-center py-10 bg-gray-100">
+      <h1 className="text-4xl font-bold mb-2">Mon Portfolio</h1>
+      <p className="text-xl text-gray-600">Développeur Web Passionné</p>
+    </header>
+  )
+}
+
+export default Header
