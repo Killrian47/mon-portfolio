016402877bbabf2093d15b7d3ccfa8100d578f03
Killian Portier <killian.portier@yahoo.fr>
2025-01-07 11:21:25 +0100

    feat: Adapt the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,12 @@ const AboutSection = () => {
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-semibold mb-6 text-center">À Propos de Moi</h2>
         <div className="max-w-3xl mx-auto text-center">
+          <p className="text-lg mb-6 font-bold">Bonjour !</p>
           <p className="text-lg mb-6">
-            Bonjour ! Je suis un développeur web passionné avec une solide expérience dans la création d'applications web modernes et performantes. J'aime relever de nouveaux défis et apprendre constamment de nouvelles technologies pour rester à la pointe de l'industrie.
+            Je suis un alternant en développement web passionné par la création d'applications modernes et performantes. Actuellement en apprentissage, je suis constamment à la recherche de nouvelles opportunités pour approfondir mes compétences et découvrir de nouvelles technologies afin de rester à jour avec les évolutions du secteur.
           </p>
           <p className="text-lg">
-            Que ce soit pour développer des interfaces utilisateur interactives, concevoir des API robustes ou optimiser les performances des applications, je m'efforce toujours de livrer un travail de haute qualité qui répond aux besoins des utilisateurs et des entreprises.
+            Que ce soit pour développer des interfaces utilisateur interactives, concevoir des API robustes ou optimiser les performances d'applications, je m'investis pleinement pour livrer des solutions de qualité tout en continuant à apprendre et à m'améliorer. Je suis motivé par l'idée de relever de nouveaux défis et de contribuer à des projets innovants qui répondent aux besoins des utilisateurs et des entreprises.
           </p>
         </div>
       </div>
